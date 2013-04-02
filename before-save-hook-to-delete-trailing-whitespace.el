e632faa344cd46dab284794ca257f0d8c777711b
SixArm <sixarm@sixarm.com>
2013-04-02 16:55:32 -0700

    Improve comment

--- before-save-hook-to-delete-trailing-whitespace.el
+++ before-save-hook-to-delete-trailing-whitespace.el
@@ -1,2 +1,2 @@
-;; Delete trailing whitespace when saving a file
+;; A before-save-hook that will delete trailing whitespace when saving a file
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
