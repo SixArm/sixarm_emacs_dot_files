5bd723f0dc6162296f3bdb508c53cca4dee9ae3c
SixArm <sixarm@sixarm.com>
2013-04-02 19:29:58 -0700

    Add link

--- indent-tabs-mode-off.el
+++ indent-tabs-mode-off.el
@@ -1,2 +1,5 @@
 ;; indent-tabs-mode-off: indent lines with spaces, not tabs.
+;;
+;; http://www.gnu.org/software/emacs/emacs-lisp-intro/html_node/Indent-Tabs-Mode.html
+
 (setq-default indent-tabs-mode nil)
