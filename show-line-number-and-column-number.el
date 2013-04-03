a2bf77c0ad50f8942208d6f6a94fd4b7707c7090
SixArm <sixarm@sixarm.com>
2013-04-02 17:26:17 -0700

    Add show-line-number-and-column-number.el

---
+++ show-line-number-and-column-number.el
@@ -0,0 +1,5 @@
+;; Show the current line number and column number in the mode line,
+;; which is typically the status line near the bottom of the screen.
+
+(setq-default line-number-mode t)
+(setq-default column-number-mode t)
