e59c54d879d0a93eddd1abb3d8bc8d1ff7739915
SixArm <sixarm@sixarm.com>
2013-04-02 17:08:20 -0700

    Add backup-inhibited.el

---
+++ backup-inhibited.el
@@ -0,0 +1,4 @@
+;; Backup inhibited: disable automatic file backups
+;; From the emacs docs: if this variable is non-nil, backups are inhibited. 
+;; This is a permanent local, so that changing the major mode does not lose its value. 
+(setq backup-inhibited t)
