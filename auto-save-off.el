a079666e0b38ef5a6578580b1aaddbc431a4e175
SixArm <sixarm@sixarm.com>
2013-04-02 17:08:30 -0700

    Add auto-save-off.el

---
+++ auto-save-off.el
@@ -0,0 +1,11 @@
+;; Turn off auto save
+;;
+;; By default, Emacs automatically saves your changes to a file intermittently. 
+;; If anything should happen, you can recover a file with ‘M-x recover-file’.
+;;
+;; Auto-saving can be turned on globally or on a per-buffer basis 
+;; with ‘M-x auto-save-mode’. 
+;;
+;; Note that auto-saving and backing up are different concepts and serve different purposes.
+
+(setq auto-save-default nil)
