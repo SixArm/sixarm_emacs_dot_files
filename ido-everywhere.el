e1e82e5e1ad6f27912a9a543bde25502ae4f2835
SixArm <sixarm@sixarm.com>
2014-03-10 18:23:59 -0700

    Create ido-everywhere.el

---
+++ ido-everywhere.el
@@ -0,0 +1,5 @@
+;; Enable ido mode everywhere.
+;; See http://www.masteringemacs.org/articles/2010/10/10/introduction-to-ido-mode/
+(setq ido-enable-flex-matching t)
+(setq ido-everywhere t)
+(ido-mode 1)
