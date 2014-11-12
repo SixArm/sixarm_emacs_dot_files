d6f8e6cbeb3595785025186f6acdf8d796405127
Joel Parker Henderson <joel@joelparkerhenderson.com>
2014-11-11 23:22:38 -0700

    Optimize startup speed

--- elisp/package-list.el
+++ elisp/package-list.el
@@ -1,6 +1,10 @@
-;; Package installation
+;; Install our favorite packages
 
+;; Require
 (require 'package)
+(require 'cl)
+
+;; Add package archives
 (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
@@ -9,55 +13,60 @@
 ;; Activate all the packages
 (package-initialize)
 
-;; Fetch the list of packages available
-(unless package-archive-contents
-  (package-refresh-contents))
-
 ;; We want these packages
-(setq package-list '(
-                     ;;starter-kit
-                     ;;starter-kit-bindings scpaste
-                     ;;starter-kit-eshell
-                     ;;starter-kit-lisp
-                     ack-and-a-half
-                     auctex
-                     clojure-mode
-                     clojure-test-mode
-                     coffee-mode
-                     deft
-                     expand-region
-                     gist
-                     groovy-mode
-                     haml-mode
-                     haskell-mode
-                     helm
-                     helm-projectile
-                     inf-ruby
-                     inf-ruby
-                     magit
-                     ;;magithub
-                     markdown-mode
-                     marmalade
-                     oddmuse
-                     paredit
-                     projectile
-                     python
-                     rainbow-mode
-                     rainbow-mode
-                     sass-mode
-                     scpaste
-                     scss-mode
-                     ;;solarized-theme
-                     tuareg
-                     volatile-highlights
-                     yari
-                     yasnippet
-                     ;;zenburn-theme
-                     yaml-mode
-                     ))
+(defvar package-list '(
+                       ;;starter-kit
+                       ;;starter-kit-bindings scpaste
+                       ;;starter-kit-eshell
+                       ;;starter-kit-lisp
+                       ack-and-a-half
+                       auctex
+                       clojure-mode
+                       clojure-test-mode
+                       coffee-mode
+                       deft
+                       expand-region
+                       gist
+                       groovy-mode
+                       haml-mode
+                       haskell-mode
+                       helm
+                       helm-projectile
+                       inf-ruby
+                       magit
+                       markdown-mode
+                       marmalade
+                       oddmuse
+                       paredit
+                       projectile
+                       python
+                       rainbow-mode
+                       rainbow-mode
+                       sass-mode
+                       scpaste
+                       scss-mode
+                       ;;solarized-theme
+                       tuareg
+                       volatile-highlights
+                       yari
+                       yasnippet
+                       yaml-mode
+                       ))
 
-(dolist (p package-list)
-  (when (not (package-installed-p p))
-    (package-install p)
-  )
-)
+;; Is every package in our list already installed?
+;; This helps us speed up startup, because if all the
+;; packages are already installed then we can skip refresh
+(defun package-list-installed-p ()
+  (loop for p in package-list
+        when (not (package-installed-p p)) do (return nil)
+        finally (return t)))
+
+;; Install packages as needed
+(unless (package-list-installed-p)
+  (package-refresh-contents)
+  (dolist (p package-list)
+    (when (not (package-installed-p p))
+      (package-install p)
+      )))
+
+(provide 'package-list)
