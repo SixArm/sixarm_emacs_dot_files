0e6c1166c481c679269f9d37f3f1bf5599377cb8
Joel Parker Henderson <joel@joelparkerhenderson.com>
2014-11-29 22:52:02 -0700

    Add file name shadow adjuster

---
+++ emacs.d/elisp/file-name-shadow-adjuster.el
@@ -0,0 +1,7 @@
+;; When selecting a file to visit, adjust paths.
+;;
+;;    "//" adjsuts to "/"
+;;    "~" adjusts to $HOME regardless of preceding text.
+
+(setq file-name-shadow-tty-properties '(invisible t))
+(file-name-shadow-mode 1)
