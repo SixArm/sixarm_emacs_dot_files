b92c187ad924987be0197c6f69812085e711975b
Joel Parker Henderson <joel@joelparkerhenderson.com>
2014-11-29 22:02:19 -0700

    Add more line number and column number settings

--- emacs.d/elisp/show-line-number-and-column-number.el
+++ emacs.d/elisp/show-line-number-and-column-number.el
@@ -1,5 +1,11 @@
-;; show-line-number-and-column-number: add this info to the mode line.
+;; show-line-number-and-column-number: show the cursor position on the mode line.
 ;; The mode line is typically the status line near the bottom of the screen.
 
 (setq-default line-number-mode t)
 (setq-default column-number-mode t)
+
+(setq line-number-mode t)
+(setq column-number-mode t)
+
+(line-number-mode t)
+(column-number-mode t)
