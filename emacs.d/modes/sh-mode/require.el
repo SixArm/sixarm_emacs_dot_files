64fbf7d4d66bd10a7cb482918b3e72761bdee133
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-06-12 20:40:28 -0600

    Exempt require sh-mode

--- emacs.d/modes/sh-mode/require.el
+++ emacs.d/modes/sh-mode/require.el
@@ -1,1 +1,6 @@
-(require 'sh-mode)
+;; This require is disabled for now, because sh-mode is not
+;; available via a package manager, as far as we know.
+;;
+;; Instead, we use the `sh-script` file in this same directory.
+
+;;(require 'sh-mode)
