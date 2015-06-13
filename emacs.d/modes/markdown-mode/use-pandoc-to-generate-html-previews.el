504d64fbcc65755b7e218c1b38854bb6a4573c6c
Joel Parker Henderson <joel@joelparkerhenderson.com>
2015-06-12 20:17:37 -0600

    Pause custom pandoc css

--- emacs.d/modes/markdown-mode/use-pandoc-to-generate-html-previews.el
+++ emacs.d/modes/markdown-mode/use-pandoc-to-generate-html-previews.el
@@ -2,4 +2,4 @@
 (setq markdown-command "pandoc --smart -f markdown -t html")
 
 ;; Use a custom css file to make it a little prettier.
-(setq markdown-css-path (expand-file-name "markdown.css" abedra/vendor-dir))
+;;(setq markdown-css-path (expand-file-name "markdown.css" abedra/vendor-dir))
