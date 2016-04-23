49803bf953dbf6fc79d0532d76b2ecd6e18ef552
Joel Parker Henderson <joel@joelparkerhenderson.com>
2016-04-23 15:58:39 -0400

    Fix indent paran

--- emacs.d/modes/sh-mode/init.el
+++ emacs.d/modes/sh-mode/init.el
@@ -3,4 +3,4 @@
 (load-file (concat dotfiles-dir "modes/sh-mode/customize.el"))
 (load-file (concat dotfiles-dir "modes/sh-mode/hooks.el"))
 (load-file (concat dotfiles-dir "modes/sh-mode/indent.el"))
-(load-file (concat dotfiles-dir "modes/sh-mode/sh-script.el")
+(load-file (concat dotfiles-dir "modes/sh-mode/sh-script.el"))
