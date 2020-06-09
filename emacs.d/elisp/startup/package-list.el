67fe834ae5af3a486c8db630eafe12e22ffd0978
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-06-09 11:28:42 -0700

    Add HTTPS for packages

--- emacs.d/elisp/startup/package-list.el
+++ emacs.d/elisp/startup/package-list.el
@@ -8,9 +8,9 @@
 (load "package")
 
 ;; Add package archives
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
 
 ;; Activate all the packages
 ;;
